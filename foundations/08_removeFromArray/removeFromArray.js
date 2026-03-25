62a0b305e823605dfc75d7edf3c66773af9cf77b
10Ascdend09 <123456789+mickiewiczszymonsluzbowy1009@gmail.com>
2026-03-25 14:07:19 +0100

    Solved another exercise using rest parameters

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...valuesToRemove) {
+
+    let arrAfterRemoval = arr.filter(
+        element => {
+            if (valuesToRemove.indexOf(element)=== -1) {
+            return true
+        }
+        else {
+            return false
+        }})
+    return arrAfterRemoval
 };
+
+console.log(removeFromArray([1,2,2,6,4,5,3],2,3,9));
 
 // Do not edit below this line
 module.exports = removeFromArray;
