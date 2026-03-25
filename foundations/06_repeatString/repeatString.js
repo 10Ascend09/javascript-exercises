55568f8236762899aaa7b2071dc63fdc52fe2ab4
10Ascdend09 <123456789+mickiewiczszymonsluzbowy1009@gmail.com>
2026-03-25 13:19:31 +0100

    Solution for exercise number 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeats) {
+    let repeatedString = "";
+    if (numberOfRepeats < 0) {
+        repeatedString += "ERROR";
+    }
+    else{
+        for (let i = 0; i < numberOfRepeats; i++) {
+        repeatedString += string;
+        }
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
