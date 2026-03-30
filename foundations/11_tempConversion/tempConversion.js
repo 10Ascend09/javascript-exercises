8a2face62474f43869ff261297295ad04772ac51
10Ascdend09 <123456789+mickiewiczszymonsluzbowy1009@gmail.com>
2026-03-30 10:13:24 +0200

    Solution for temperature Conversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  let degreesCelsius = (degreesFahrenheit - 32) * 5 / 9;
+  return Math.round(10*degreesCelsius) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  let degreesFahrenheit = (degreesCelsius * 9 / 5) + 32;
+  return Math.round(10*degreesFahrenheit) / 10
 };
 
 // Do not edit below this line
