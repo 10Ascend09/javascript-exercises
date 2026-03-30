5046fc0f728288541ecaec30b8aa1f73d5ebf3f8
10Ascdend09 <123456789+mickiewiczszymonsluzbowy1009@gmail.com>
2026-03-30 09:51:29 +0200

    Provided sollution to exercise of summing values between two integers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(rangeNumber, rangeNumber2) {
+    console.log(rangeNumber);
+    console.log(typeof rangeNumber);
+    console.log(rangeNumber2);
+    console.log(typeof rangeNumber2);
+    
+
+    if (Number(rangeNumber) && Number(rangeNumber2) 
+        && rangeNumber >= 0 && rangeNumber2 >= 0 
+        && rangeNumber === Math.floor(rangeNumber)
+        && rangeNumber2 === Math.floor(rangeNumber2)) {
+            
+        let summedNumber = 0;
+        if (rangeNumber < rangeNumber2) {
+        for (let i = rangeNumber; i <= rangeNumber2; i++) {
+            summedNumber += i;
+        }
+        return summedNumber
+    } else if (rangeNumber > rangeNumber2) {
+        for (let i = rangeNumber2; i <= rangeNumber; i++) {
+            summedNumber += i;
+        }
+        return summedNumber
+    }
+    } else return "ERROR"
+
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+Math.abs()
