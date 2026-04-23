9f58bb0c025c453a16fde3ef2b66cbbd6db1a847
10Ascdend09 <123456789+mickiewiczszymonsluzbowy1009@gmail.com>
2026-04-23 11:10:33 +0200

    Solution for finding the oldest person in array

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    const currentDate = new Date();
 
+    peopleArray.sort((personA,personB)=>{
+        let agePersonA = (personA.yearOfDeath ? personA.yearOfDeath : currentDate.getFullYear()) -personA.yearOfBirth;
+        let agePersonB = (personB.yearOfDeath ? personB.yearOfDeath : currentDate.getFullYear()) -personB.yearOfBirth;
+        
+        if (agePersonA > agePersonB) {
+            return -1;
+        } else if (agePersonA < agePersonB) {
+            return 1;
+        }
+        return 0;
+    })
+    return peopleArray[0]
 };
+
+console.log(findTheOldest([
+    {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+          name: "Jane",
+          yearOfBirth: 1912,
+          yearOfDeath: 1941,
+        },
+    ]));
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+/*
+sort by age -> take first instance O(n)
+
+*/ 
