81e00a57342ba10f5112beaed95a875f40cc3822
10Ascdend09 <123456789+mickiewiczszymonsluzbowy1009@gmail.com>
2026-04-23 10:28:55 +0200

    Solution for fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(sequenceNumber) {
+    sequence = [1,1];
+    if (sequenceNumber === 1){
+        return sequence[0]
+    } else if (sequenceNumber === 2){
+        return sequence[1]
+    } else if (Number(sequenceNumber) === 0){
+        return 0
+    } else if (sequenceNumber > 0){
+        for (let i = 2; i < sequenceNumber; i++) {
+            sequence.push(sequence[i-2] + sequence[i-1]);
 
+        }
+        return sequence[sequenceNumber - 1]
+    } else return "OOPS"
 };
 
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(10));
 // Do not edit below this line
 module.exports = fibonacci;
